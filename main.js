7e32881c2973dc6791b9d564d5a6befc08d2dfba
HannesJoerk0174 <hannes.joerk@ottogroup.com>
2022-04-07 09:46:08 +0200

    image displays the variant on hover over color

--- main.js
+++ main.js
@@ -13,5 +13,13 @@ const app = Vue.createApp({
             ],
             sizes: ['S', 'M', 'L', 'XL' ]
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        }
     }
 })
